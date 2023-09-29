848864f4c45f4739604c75447b0d97465e4c969d
kimbohlovette <kimbohlovette@gmail.com>
2023-09-29 03:09:13 +0100

    Added game statistics to the play page

--- frontend/src/components/Play.tsx
+++ frontend/src/components/Play.tsx
@@ -14,32 +14,48 @@ const Play = () => {
 	};
 	return (
 		<div className="flex items-center justify-center">
-			<div>
-				<div className="mt-8">
-					<h1 className="text-blue-400 my-3 font-semibold text-2xl">
-						Question
-					</h1>
-					<p className="my-4 text-slate-600 font-light max-w-xl">
-						Lorem ipsum dolor sit, amet consectetur adipisicing
-						elit. Sit porro quasi, dolores quos mollitia qua?
-					</p>
+			<div className="divide-y">
+				<div className="mb-5">
+					<div className="mt-8">
+						<h1 className="text-blue-400 my-3 font-semibold text-2xl">
+							Question
+						</h1>
+						<p className="my-4 text-slate-600 font-light max-w-xl">
+							Lorem ipsum dolor sit, amet consectetur adipisicing
+							elit. Sit porro quasi, dolores quos mollitia qua?
+						</p>
+					</div>
+					<form
+						onSubmit={handleSubmit(onSubmit)}
+						className="[&>*]:block w-full max-w-lg py-5 sm:flex sm:items-center gap-x-2"
+					>
+						<input
+							defaultValue=""
+							{...register('answer')}
+							name="answer"
+							type="text"
+							placeholder="Type answer here"
+							className="flex-1 border rounded-lg w-full px-4 py-2 focus:outline-blue-400"
+						/>
+						<button className="shadow rounded-lg bg-blue-400 hover:bg-blue-500 text-white text-center mt-8 sm:mt-0 px-4 py-2 w-full sm:max-w-fit">
+							Submit
+						</button>
+					</form>
 				</div>
-				<form
-					onSubmit={handleSubmit(onSubmit)}
-					className="[&>*]:block w-full max-w-lg py-5 sm:flex sm:items-center gap-x-2"
-				>
-					<input
-						defaultValue=""
-						{...register('answer')}
-						name="answer"
-						type="text"
-						placeholder="Type answer here"
-						className="flex-1 border rounded-lg w-full px-4 py-2 focus:outline-blue-400"
-					/>
-					<button className="shadow rounded-lg bg-blue-400 hover:bg-blue-500 text-white text-center mt-8 sm:mt-0 px-4 py-2 w-full sm:max-w-fit">
-						Submit
-					</button>
-				</form>
+				<div className="py-8 flex flex-col sm:flex-row sm:[&>*]:flex-1 sm:[&>*]:justify-between [&>*]:flex gap-2">
+					<div className="py-2 px-4 rounded-md my-1 bg-slate-50">
+						<span className="text-slate-600">Wins</span>
+						<span className="text-blue-300">5</span>
+					</div>
+					<div className="py-2 px-4 rounded-md my-1 bg-slate-50">
+						<span className="text-slate-600">Attempts left</span>
+						<span className="text-blue-300">0</span>
+					</div>
+					<div className="py-2 px-4 rounded-md my-1 bg-slate-50">
+						<span className="text-slate-600">Total Rewards</span>
+						<span className="text-blue-300">543</span>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
