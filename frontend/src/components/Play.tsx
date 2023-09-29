1f14909faf392eea6e0efa2e0cf7d2dda6a2b323
kimbohlovette <kimbohlovette@gmail.com>
2023-09-29 02:27:41 +0100

    Fixed the play game page

--- frontend/src/components/Play.tsx
+++ frontend/src/components/Play.tsx
@@ -14,20 +14,19 @@ const Play = () => {
 	};
 	return (
 		<div className="flex items-center justify-center">
-			<div className="flex flex-col items-center">
-				<div className="text-center mt-8">
-					<h1 className="text-blue-700 my-3 font-medium text-2xl">
+			<div>
+				<div className="mt-8">
+					<h1 className="text-blue-400 my-3 font-semibold text-2xl">
 						Question
 					</h1>
-					<p className="my-4 text-slate-600 font-light text-lg max-w-xl">
+					<p className="my-4 text-slate-600 font-light max-w-xl">
 						Lorem ipsum dolor sit, amet consectetur adipisicing
-						elit. Sit porro quasi, dolores quos mollitia quam
-						voluptatum illum velit temporibus ipsa nam beata?
+						elit. Sit porro quasi, dolores quos mollitia qua?
 					</p>
 				</div>
 				<form
 					onSubmit={handleSubmit(onSubmit)}
-					className="[&>*]:block w-full max-w-lg py-5"
+					className="[&>*]:block w-full max-w-lg py-5 sm:flex sm:items-center gap-x-2"
 				>
 					<input
 						defaultValue=""
@@ -35,9 +34,9 @@ const Play = () => {
 						name="answer"
 						type="text"
 						placeholder="Type answer here"
-						className="border rounded-full w-full px-4 py-3 focus:outline-blue-400 text-center"
+						className="flex-1 border rounded-lg w-full px-4 py-2 focus:outline-blue-400"
 					/>
-					<button className="shadow rounded-full bg-blue-400 hover:bg-blue-600 text-white text-center mx-auto my-4 px-4 py-2 w-full max-w-xs">
+					<button className="shadow rounded-lg bg-blue-400 hover:bg-blue-500 text-white text-center mt-8 sm:mt-0 px-4 py-2 w-full sm:max-w-fit">
 						Submit
 					</button>
 				</form>
